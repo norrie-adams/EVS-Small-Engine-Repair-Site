7d9a304551402ef37d76ddcabde4a0bf6e7ad877
norrie-adams <normanadams087@gmail.com>
2026-05-25 16:38:28 -0400

    Added Login Frontend Logic

---
+++ public/login.js
@@ -0,0 +1,38 @@
+document.getElementById('loginForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const usernameInput = document.getELementById('username').value;
+    const passwordInput = document.getELementById('password').value;
+    const messageElement = document.getELementById('message');
+
+    try {
+
+        const response = await fetch('http:/localhost:8000/login', {
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: usernameInput,
+                password: passwordInput
+            })
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            messageElement.style.color = 'green';
+            messageElement.innerText = data.message;
+
+            setTimeout(() => {
+                window.location.href = '/admin.html';
+            }, 1000);
+        } else {
+            messageElement.style.color = 'red';
+            messageElement.innerText = data.message;
+        }
+    } catch (error) {
+        console.error("Network error:", error);
+        messageElement.innerText = "Something went wrong connecting to the server";
+    }
+});
