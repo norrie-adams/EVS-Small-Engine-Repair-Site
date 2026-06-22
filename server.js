efe5fac9f1161ab3fd9eb577abea6b9da18c518c
norrie-adams <normanadams087@gmail.com>
2026-06-22 10:25:24 -0400

    Folder Cleanup

--- server.js
+++ server.js
@@ -23,5 +23,5 @@ app.get('/', (req, res) => {
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server is running beautifully on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
